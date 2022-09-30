ae6846269fa956d7954f327c4421a1c23717f66c
VSacr1 <vjsacre@gmail.com>
2022-09-30 11:24:25 +0100

    Example commit

--- front-end/application/models.py
+++ front-end/application/models.py
@@ -1,7 +1,6 @@
-from application import db 
+from application import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(30))
     task_desc = db.Column(db.String(100)) 
-    
--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -1,7 +1,13 @@
-from application import app
+from application import app, db
 from flask import request, render_template, redirect, url_for
 from application.models import *
 from application.forms import *
+
+#Read 
+@app.route("/", methods=['GET', 'POST'])
+def view_all_tasks():
+    tasks = Task.query.all()
+    return render_template('home.html', tasks=tasks)
 
 #Create 
 @app.route('/add_task', methods=['GET','POST'])
@@ -9,25 +15,35 @@ def create_new_task():
     form = TaskForm()
     if form.validate_on_submit(): 
         tasks = Task(
-            taskname = form.task_name.data,
-            taskdesc = form.task_desc.data
+            task_name = form.task_name.data,
+            task_desc = form.task_desc.data
         )
         db.session.add(tasks)
         db.session.commit()
         return redirect(url_for('view_all_tasks'))
     return render_template('add.html', form=form)
 
-#Read 
-@app.route('/')
-def view_all_tasks():
-    tasks = Task.query.all()
-    return render_template('home.html', tasks=tasks)
+
 
 #Update 
+@app.route('/complete/<int:task_id>', methods=['GET', 'POST'])
+def update(task_id):
+    form = TaskForm()
+    tasks = Task.query.get(task_id)
+    if form.validate_on_submit(): 
+        tasks.task_name = form.task_name.data
+        tasks.task_desc = form.task_desc.data
+        db.session.commit()
+        return redirect(url_for('view_all_tasks'))
+    elif request.method == 'GET':
+        form.task_name.data = tasks.task_name
+        form.task_desc.data = tasks.task_desc
+    return render_template('update.html', form=form)
 
 #Delete
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
     tasks = Task.query.get(task_id)
     db.session.delete(tasks)
-    return redirect(url_for('index'))
+    db.session.commit()
+    return redirect(url_for('view_all_tasks'))
